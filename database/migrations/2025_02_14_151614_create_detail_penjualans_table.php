bf8b5a06a83cdc021536bc6a6c703ec26ec1dbfc
HizbullahHasanIbrahim <hasanjo365@gmail.com>
2025-02-14 22:29:59 +0700

    setting migration

---
+++ database/migrations/2025_02_14_151614_create_detail_penjualans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_penjulan');
+            $table->unsignedBigInteger('id_barang');
+            $table->integer('jumlah_produk');
+            $table->decimal('sub_total', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_penjualans');
+    }
+};
